4368f3c24a4ad6f437a0dc0e3e795b1b6105c4e4
Maurizio Branca <maurizio.branca@gmail.com>
2023-11-29 23:32:03 +0100

    Bump azure-eventhub to 5.11.5 (#6)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-VERSION = "0.2.1"
+VERSION = "0.2.2"
 
 
 def get_long_description():
@@ -33,7 +33,7 @@ setup(
     """,
     install_requires=[
         "click",
-        "azure-eventhub==5.11.1",
+        "azure-eventhub==5.11.5",
     ],
     extras_require={
         "test": ["pytest"]
